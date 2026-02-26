a85dead9527d061fa5b0c291ed9e4bade395c81a
wenjess1 <jessica.wen@mail.utoronto.ca>
2026-02-25 19:31:37 -0500

    quiz

--- src/users.ts
+++ src/users.ts
@@ -7,9 +7,30 @@ export const apiResponse: unknown = [
 ];
 
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  if (!Array.isArray(apiResponse)) {
+    return []; // I'm confused because the instructions say to not throw runtime errors, but I feel like we should for this case
+  }
+
+  // I dont know how to do this without doing "as User", so this is the best I can do
+  const convertToUser = apiResponse.filter(
+    (item): item is User =>
+      typeof item === "object" &&
+      "name" in item &&
+      "age" in item
+  );
+
+  return formatAges(convertToUser);
 }
 
-export function formatAges(users: User[]): string[] {
-  return users.map((u) => u.age.toFixed(0));
+export function formatAges(users: User[]): User[] {
+  return users.map((u) => {
+    let age = u.age;
+    if (typeof age === "string") {
+      age = Number(age);
+    }
+    return {
+      ...u,
+      age: age,
+    };
+  });
 }
